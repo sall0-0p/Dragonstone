80f0e9fac411983ee057509aa5f023bfdc6e7112
sall0-0p <antoniuk.orest@gmail.com>
2022-09-18 12:51:47 +0300

    Alpha 5. Some updates

--- OS/SystemApps/Desktop/test.lua
+++ OS/SystemApps/Desktop/test.lua
@@ -1,15 +1,11 @@
 local basalt = require(".UwUntuCC.OS.Libraries.Basalt")
 
-local main = basalt.createFrame("firstBaseFrame")
-local main2 = basalt.createFrame("secondBaseFrame"):hide():setBackground(colors.black)
-main:addButton()
-  :setText("Show")
-  :onClick(function()
-      local frame2 = basalt.getFrame("secondBaseFrame")
-      if(frame2~=nil)then
-            basalt.debug("DEBUGGGGGss")
-            frame2:show()
-      end
-  end)
+local mainFrame = basalt.createFrame()
+
+local button = mainFrame:addButton()
+  :setPosition(2,2)
+  :onHover(function()
+    button:setBackground)
+  )
 
   basalt.autoUpdate()
