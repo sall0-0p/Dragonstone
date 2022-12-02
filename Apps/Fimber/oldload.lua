697118f2339ad28929e77d9bf3698e52cb582bf5
sall0-0p <antoniuk.orest@gmail.com>
2022-12-02 09:30:18 +0200

    -

---
+++ Apps/Fimber/oldload.lua
@@ -0,0 +1,51 @@
+local basalt = require("basalt")
+local main = basalt.createFrame()
+
+local files = fs.list("/")
+
+
+local fileObjects = {}
+local fileList = main:addFrame():setSize(35, 12):setScrollable()
+
+local function updateFiles(selected)
+    for k,v in pairs(fileObjects)do
+        v.label:setPosition(1,1):hide()
+        v.pane:setPosition(1,1):hide()
+    end
+    local rColoring = false
+    local y = 1
+
+    local function udpateItem(group, text, bg, fg)
+        if(selected==y)then
+            group.pane:setPosition(1, y):setBackground(colors.red):show()
+            group.label:setText(text):setPosition(3, y):setBackground(colors.red):setForeground(colors.black):show()
+        else
+            group.pane:setPosition(1, y):setBackground(bg):show()
+            group.label:setText(text):setPosition(3, y):setBackground(bg):setForeground(fg):show()
+        end
+        y = y + 1
+    end
+
+    for k,v in pairs(files)do
+        if(fileObjects[y]~=nil)then
+            udpateItem(fileObjects[y], v, rColoring and colors.black or colors.white, rColoring and colors.white or colors.black)
+        else
+            local group = {}
+            group.pane = fileList:addPane():setSize("parent.w", 1)
+            group.label = fileList:addLabel()
+            udpateItem(group, v, rColoring and colors.black or colors.white, rColoring and colors.white or colors.black)
+            group.pane:onClick(function()
+                updateFiles(group.pane:getY())
+            end)
+            table.insert(fileObjects, group)
+        end
+        rColoring = not rColoring
+    end
+end
+updateFiles()
+
+local btn = main:addButton():setPosition(38, 5):onClick(function()
+    updateFiles(math.random(1,8))
+end)
+
+basalt.autoUpdate()
